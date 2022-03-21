efb1110ffecb4ff8ec7ad93067c3c0f5dc260345
Raazia Tariq <89384079+RaaziaTarique@users.noreply.github.com>
2022-03-21 11:35:23 +0100

    - modifications in gradle

--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -51,7 +51,8 @@ android {
   sourceSets { getByName("test").apply { resources.setSrcDirs(listOf("testdata")) } }
 
   tasks.withType<Test>().configureEach {
-    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() - 1).takeIf { it > 0 } ?: 1
+    println ("MAX PARALLEL FORKS  $maxParallelForks")
     setForkEvery(100)
     testLogging.showStandardStreams = true
     minHeapSize = "512m"
